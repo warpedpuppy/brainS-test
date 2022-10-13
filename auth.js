a0bd537df77d72a47fbc9990b52199e1e1a0ea60
Brian Smawley <briansmawley@gmail.com>
2022-10-13 11:00:31 -0400

    Authentication & Authorization
    inclusion of auth.js and passport.js to facilitate security of app

--- auth.js
+++ auth.js
@@ -1,33 +1,36 @@
-const jwtSecret = "your_jwt_secret";
+const jwtSecret = 'your_jwt_secret'; 
 
-const jwt = require("jsonwebtoken"),
-  passport = require("passport");
+const jwt = require('jsonwebtoken'),
+  passport = require('passport');
 
-require("./passport");
+require('./passport'); 
+
 
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
-    subject: user.Username,
-    expiresIn: "7d",
-    algorithm: "HS256",
+    subject: user.name,
+    expiresIn: '7d',
+    algorithm: 'HS256' 
   });
-};
+}
+
 
 module.exports = (router) => {
-  router.post("/login", (req, res) => {
-    passport.authenticate("local", { session: false }, (error, user, info) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
-          message: "Something is not right",
-          user: user,
+          message: 'Something is not right',
+          user: user
         });
       }
       req.login(user, { session: false }, (error) => {
         if (error) {
           res.send(error);
         }
-        let token = generateJWTToken(user.toJson());
+        let token = generateJWTToken(user.toJSON());
+        return res.json({ user, token });
       });
     })(req, res);
   });
-};
+}
