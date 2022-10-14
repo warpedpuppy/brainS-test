a9f4d627b4eddd9a1a88f1e698023584c150c183
ted <ted@warpedpuppy.com>
2022-10-14 18:21:31 -0400

    fixed connection

--- index.js
+++ index.js
@@ -308,6 +308,6 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 8080;
 // app = express();
-app.listen("port", port,() => {
+app.listen(port,() => {
  console.log('Listening on Port ' + port);
 });
