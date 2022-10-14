7b9a0e96a5148ab73ec38126fe0d5deff9abb5e2
ted <ted@warpedpuppy.com>
2022-10-14 17:58:34 -0400

    removed line 310 of index

--- index.js
+++ index.js
@@ -307,7 +307,7 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 8080;
-app = express();
+// app = express();
 app.listen("port", PORT,() => {
  console.log('Listening on Port ' + port);
 });
